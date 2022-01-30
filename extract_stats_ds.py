836544a7133b2480de23f6a869092eca77742a09
camgbus <camilag.bustillo@gmail.com>
2022-01-30 18:44:50 +0100

    Extract from nnUNet paths

--- extract_stats_ds.py
+++ extract_stats_ds.py
@@ -1,7 +1,4 @@
-import os
-from statistics import mean
-import numpy as np
-import SimpleITK as sitk
+import os, argparse
 from ds_info.utils.io_utils import list_files
 from ds_info.feature_extraction.feature_combinations import reduced_features, props_mean_std
 from ds_info.visualization.plots import props_to_pandas
@@ -27,3 +24,15 @@ def save_ds_stats(dataset_path, save_df_path, ds_name, img_mode=None, case_names
     df = props_to_pandas(props, columns=['intensity_mean', 'resolution', 'spacing', '1_area-rel', '1_CC'])
     csv_name = "{}_stats.csv".format(ds_name)
     df.to_csv(os.path.join(save_df_path, csv_name))    
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("task")
+    parser.add_argument("save_path")
+    args = parser.parse_args()
+    dataset_path = os.path.join(os.environ['nnUNet_raw_data_base'], 'nnUNet_raw_data', args.task, 'imagesTr')
+    save_df_path = args.save_path
+    save_ds_stats(dataset_path, save_df_path, ds_name='HipSubset', img_mode=None, case_names=None)
+
+if __name__ == "__main__":
+    main()
