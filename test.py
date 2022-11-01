c26580826ab9142645a83f492b653b6d076c5932
camgbus <camilag.bustillo@gmail.com>
2022-11-02 00:31:48 +0100

    Start decomposition

---
+++ test.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def extract_center_array(lengths):
+    """Extract the tensor in the middle of the inputted tensor.
+    """
+
+def smallest_common_dimensions():
+    """Returns the smallest common dimensions in x, y and z directions.
+    """
+
+    return (x_length, y_length, z_length)
+
+def PCA
+
+x = np.array([[[0, 1, 2], [3, 4, 5], [6, 7, 8]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]], [[20, 21, 22], [23, 24, 25], [26, 27, 28]]])
+
+
+print(unique)
+print(counts)
